6cabf2458601afbd1178ab4b3dd31b46f5672682
ddddzzoo <873720581@qq.com>
2023-11-17 00:13:03 +0800

    完善MyString类

--- Learning/MoveSemantic.cpp
+++ Learning/MoveSemantic.cpp
@@ -1,22 +1,6 @@
-#include <iostream>
-
 #include "MyString.h"
 
-void move(char x, char y) { printf("%c --> %c\n", x, y); }
-
-void hanoi(int n, char one, char two, char three) {
-  if (n == 1) {
-    move(one, three);
-  }
-  else {
-    hanoi(n - 1, one, three, two);
-    move(one, three);
-    hanoi(n - 1, two, one, three);
-  }
-}
-
 int main() {
-  int m;
-
+  test();
   return 0;
 }
--- Learning/MyString.h
+++ Learning/MyString.h
@@ -9,12 +9,22 @@ class MyString {
     strcpy(_pstr, pstr);
   }
 
+  // MyString s2 = s1
   MyString(const MyString& rhs) : _pstr(new char[strlen(rhs._pstr) + 1]) {
     std::cout << "MyString(const MyString&)" << std::endl;
     strcpy(_pstr, rhs._pstr);
   }
 
-
+  MyString& operator=(const MyString& rhs) {
+    std::cout << "MyString& operator=(const MyString &)" << std::endl;
+    if (this != &rhs) {  // 防止自我复制
+      delete[] _pstr;
+      _pstr = nullptr;
+      _pstr = new char[strlen(rhs._pstr) + 1]();  // 深拷贝
+      strcpy(_pstr, rhs._pstr);
+    }
+    return *this;
+  }
 
   // (传递右值时)具有移动语义的函数优先具有复制语义的函数
   // 移动构造函数、移动赋值运算符函数称为具有移动语义函数
@@ -36,9 +46,50 @@ class MyString {
 
     return *this;  // 返回*this
   }
-  // ......
+
+  ~MyString() {
+    std::cout << "~MyString()" << std::endl;
+    if (_pstr) {
+      delete[] _pstr;
+      _pstr = nullptr;
+    }
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const MyString& rhs);
+
  private:
   char* _pstr;
 };
 
-void test() { MyString s1("Hello"); }
+std::ostream& operator<<(std::ostream& os, const MyString& rhs) {
+  if (rhs._pstr) {
+    os << rhs._pstr;
+  }
+  return os;
+}
+
+void test() {
+  MyString s1("Hello");  // MyString(const char*)
+  std::cout << "s1 = " << s1 << std::endl;
+
+  std::cout << std::endl;
+  MyString s2 = s1;  // MyString(const MyString&)
+  std::cout << "s1 = " << s1 << std::endl;
+  std::cout << "s2 = " << s2 << std::endl;
+
+  std::cout << std::endl;
+  MyString s3 = "world";  // MyString(const char*)
+  std::cout << "s3 = " << s3 << std::endl;
+
+  std::cout << std::endl;
+  s2 = MyString("world");  // MyString &operator= (MyString&& rhs)
+  // MyString("world") 右值，临时对象会短暂存在栈上
+  std::cout << "s2 = " << s2 << std::endl;
+
+  std::cout << std::endl;
+  MyString s4 = "****";
+  // std::move将左值转换为右值
+  // 实际上发生了强制转换static_cast<T &&>(lvalue)
+  s2 = std::move(s4);  // MyString &operator= (MyString&& rhs)
+  std::cout << "s2 = " << s2 << std::endl;
+}
